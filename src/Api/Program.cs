fc5bc2da45005dfe5ab17fd77f86740607e8ffe0
JC <jcdotnet@hotmail.com>
2026-04-14 14:06:10 +0200

    feat: implement structured logging with Serilog

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,49 +1,71 @@
 using Api.Infrastructure;
 using Application;
 using Infrastructure;
+using Serilog;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-// Add services to the container.
+try
+{
+    Log.Information("Starting JcDotNet Clean Architecture API...");
 
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
+    // Add serilog as the logging provider
+    builder.Host.UseSerilog((context, config) => config
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+    // Add services to the container.
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
+
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            // Display Enums as strings in the JSON responses
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(config =>
     {
-        // Display Enums as strings in the JSON responses
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        config.SwaggerDoc("v1", new() { Title = "JcDotNet Clean Architecture API", Version = "v1" });
     });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(config =>
-{
-    config.SwaggerDoc("v1", new() { Title = "JcDotNet Clean Architecture API", Version = "v1" });
-});
+    var app = builder.Build();
 
-var app = builder.Build();
+    app.UseExceptionHandler();
 
-app.UseExceptionHandler();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(config =>
+    if (app.Environment.IsDevelopment())
     {
-        config.SwaggerEndpoint("/swagger/v1/swagger.json", "JcDotNet API V1");
-        config.RoutePrefix = string.Empty; // This makes Swagger the home page
-    });
+        app.UseSwagger();
+        app.UseSwaggerUI(config =>
+        {
+            config.SwaggerEndpoint("/swagger/v1/swagger.json", "JcDotNet API V1");
+            config.RoutePrefix = string.Empty; // This makes Swagger the home page
+        });
+    }
+
+    // Configure the HTTP request pipeline.
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "The application failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
