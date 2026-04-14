8862eec328ea28349eccaab9f557724a3412c160
JC <jcdotnet@hotmail.com>
2026-04-14 12:04:49 +0200

    feat: add MediatR and register application services in Program.cs

---
+++ src/Application/Features/ProjectTasks/Commands/CreateProjectTask/CreateProjectTaskCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using MediatR;
+
+namespace Application.Features.ProjectTasks.Commands.CreateProjectTask
+{
+    // The Command: What data do we need to create a task?
+    public record CreateProjectTaskCommand(
+        string Title,
+        string Description,
+        PriorityLevel Priority,
+        DateTime? DueDate) : IRequest<Guid>;
+}
---
+++ src/Application/Features/ProjectTasks/Commands/CreateProjectTask/CreateProjectTaskCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.ProjectTasks.Commands.CreateProjectTask
+{
+    public class CreateProjectTaskCommandHandler : IRequestHandler<CreateProjectTaskCommand, Guid>
+    {
+        public Task<Guid> Handle(CreateProjectTaskCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new ProjectTask(
+                request.Title,
+                request.Description,
+                request.Priority,
+                request.DueDate);
+
+            // TO-DO later (when we have a DB)
+            // _repository.Add(entity);
+            // await _unitOfWork.SaveChangesAsync();
+
+            return Task.FromResult(entity.Id);
+        }
+    }
+}
