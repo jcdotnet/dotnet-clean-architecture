68af9256a6979f6f5c8d22b28357b25c6ff13d2c
JC <jcdotnet@hotmail.com>
2026-04-14 13:06:17 +0200

    feat: add ProjectTasks controller using ISender and primary constructors

---
+++ src/Api/Controllers/ProjectTasksController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.ProjectTasks.Commands.CreateProjectTask;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectTasksController(ISender mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create(CreateProjectTaskCommand command)
+        {
+            var id = await mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
