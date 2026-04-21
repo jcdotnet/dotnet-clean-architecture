e595050b32f38bec8b01997ee465f360a7a767ee
JC <jcdotnet@hotmail.com>
2026-04-21 10:55:51 +0200

    refactor(app): inject ProjectTaskMapper into query handlers

--- src/Application/Features/ProjectTasks/Queries/GetProjectTasks/GetProjectTasksQueryHandler.cs
+++ src/Application/Features/ProjectTasks/Queries/GetProjectTasks/GetProjectTasksQueryHandler.cs
@@ -4,12 +4,9 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.ProjectTasks.Queries.GetProjectTasks;
-public class GetProjectTasksQueryHandler(IApplicationDbContext context) :
+public class GetProjectTasksQueryHandler(IApplicationDbContext context, ProjectTaskMapper mapper) :
     IRequestHandler<GetProjectTasksQuery, IEnumerable<ProjectTaskDto>>
-{
-
-    private readonly ProjectTaskMapper _mapper = new();
-    
+{   
     public async Task<IEnumerable<ProjectTaskDto>> Handle(GetProjectTasksQuery request, 
         CancellationToken cancellationToken)
     {
@@ -18,7 +15,7 @@ public class GetProjectTasksQueryHandler(IApplicationDbContext context) :
         .AsNoTracking()
         .ToListAsync(cancellationToken);
 
-        return tasks.Select(_mapper.ProjectTaskToDto);
+        return tasks.Select(mapper.ProjectTaskToDto);
 
     }
 }
